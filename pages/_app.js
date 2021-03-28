6c9a20e2803f5a8401c1d3e06acf2873cdb99927
Tomy Smith <Thomas.Smith@skybettingandgaming.com>
2021-03-28 23:20:07 +0100

    Remove Inter font

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,7 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;600&display=swap"
-        rel="stylesheet"
-      />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
